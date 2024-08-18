1eec7c792667f7b92915edd1b2b248c6e9c84bb6
Mario162003 <macarce2003@gmail.com>
2024-08-18 11:15:40 +0200

    EDA_final_project

--- src/app.py
+++ src/app.py
@@ -2,3 +2,10 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Cargar variables de entorno desde el archivo .env
+
+database_url = os.getenv('DATABASE_URL')
+print(database_url)
